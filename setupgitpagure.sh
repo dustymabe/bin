7a109f846ce5401c09f32a770b2660292495c168
Dusty Mabe <dusty@dustymabe.com>
2018-03-19 13:51:29 -0400

    setupgitpagure: add $DOMAIN var

--- setupgitpagure.sh
+++ setupgitpagure.sh
@@ -1,13 +1,13 @@
 
 # To set up pagure instead
 REPO=$1
-git clone https://pagure.io/forks/dustymabe/${REPO}.git
-pushd $REPO
-git remote set-url --push origin ssh://git@pagure.io/forks/dustymabe/${REPO}.git
-git remote add upstreamorigin https://pagure.io/${REPO}.git
+DOMAIN=${2-pagure.io}
+git clone https://${DOMAIN}/forks/dustymabe/${REPO}.git
+pushd $(basename $REPO)
+git remote set-url --push origin ssh://git@${DOMAIN}/forks/dustymabe/${REPO}.git
+git remote add upstreamorigin https://${DOMAIN}/${REPO}.git
 git fetch upstreamorigin
 git checkout -t remotes/upstreamorigin/master -b upstreammaster
 git checkout master
 popd
-
-
+exit
